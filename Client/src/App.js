3aae9f41c7824d33b6cef3e69c8bdb2a8cc72655
Shaik Jani Shaida <96036214+jani474747@users.noreply.github.com>
2022-09-27 10:11:59 +0530

    project for my reference

---
+++ Client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {FormData} from "./components/FormData"
+
+
+
+function App() {
+  return (
+   <div className='App'>
+   <FormData/>
+   </div>
+  )
+}
+
+export default App;
